809bcc076e973b16497fc2d60cfe37f71aa7d672
Sathish Babu <sathish.babu@zohocorp.com>
2020-09-23 16:14:14 +0530

    :building_construction: Moved db operations to separate file

---
+++ database.py
@@ -0,0 +1,15 @@
+import os
+
+
+def make_db_connection(app):
+    sqlalchemy_database_uri = 'mysql+pymysql://{username}:{password}@{hostname}/{databasename}'.format(
+        username=os.environ.get('DB_USERNAME'),
+        password=os.environ.get('DB_PASSWORD'),
+        hostname='remotemysql.com',
+        databasename=os.environ.get('DB_NAME'),
+    )
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = sqlalchemy_database_uri
+    app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    return app
